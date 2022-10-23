7677f05ba54c680c63849bed51bb673b9b97708c
hickey <hxjwork666@163.com>
2022-10-23 14:10:56 +0800

    func method

---
+++ func_method/src/main.rs
@@ -0,0 +1,87 @@
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// 实现的代码块，`Point` 的所有方法都在这里给出
+impl Point {
+    // 这是一个静态方法（static method）
+    // 静态方法不需要被实例调用
+    // 这类方法一般用作构造函数（constructor）
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    // 另外一个静态方法，需要两个参数：
+    fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y }
+    }
+}
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+impl Rectangle {
+    // 这是一个实例方法（instance methond）
+    // `&self` 是 `self: &Self` 的语法糖（sugar），其中 `Self` 是方法的调用者的类型，在这个例子中 `Self` = `Rectangle`
+    fn area(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+
+    fn perimeter(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+    }
+
+    // 这个方法要求调用者是可变的，`&mut self` 为 `self: &mur Self` 的语法糖
+    fn translate(&mut self, x: f64, y: f64) {
+        self.p1.x += x;
+        self.p2.x += x;
+
+        self.p1.y += y;
+        self.p2.y += y;
+    }
+}
+
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+    // 这个方法会 “消耗” 调用者的资源
+    fn destroy(self) {
+        let Pair(first, second) = self;
+
+        println!("Destroy Pair({}, {})", first, second);
+
+        // `first` 和 `second` 离开作用域后释放
+    }
+}
+fn main() {
+    let rectangle = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    // 实例方法通过点运算符来调用
+    // 注意第一个参数 `&self` 是隐式传递的
+    // `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+}
