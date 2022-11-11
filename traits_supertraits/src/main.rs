a34965a842095693b31335522ab159c6f423b633
hickey <hxjwork666@163.com>
2022-11-11 17:11:09 +0800

    traits supertraits

---
+++ traits_supertraits/src/main.rs
@@ -0,0 +1,26 @@
+trait Person {
+    fn name(&self) -> String;
+}
+
+trait Student: Person {
+    fn university(&self) -> String;
+}
+
+trait Program {
+    fn fav_language(&self) -> String;
+}
+
+trait CompSciStudent: Program + Student {
+    fn git_username(&self) -> String;
+}
+
+fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+    format!(
+        "My name is {} and I attend {}. My favorite language is {}, My Git username is {}",
+        student.name(),
+        student.university(),
+        student.fav_language(),
+        student.git_username()
+    )
+}
+fn main() {}
