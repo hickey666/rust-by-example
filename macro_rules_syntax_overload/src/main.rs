346a54ccdb2bb5f27081fde9ca612159015b511b
hickey <hxjwork666@163.com>
2022-11-11 21:54:06 +0800

    macro_rules syntax overload

---
+++ macro_rules_syntax_overload/src/main.rs
@@ -0,0 +1,24 @@
+// 根据你调用它的方式，`test!` 将以不同的方式来比较 `$left` 和 `$right`。
+macro_rules! test {
+    ($left:expr; and $right:expr) => {
+        println!(
+            "{:?} and {:?} is {:?}",
+            stringify!($left),
+            stringify!($right),
+            $left && $right
+        )
+    };
+
+    ($left:expr; or $right:expr) => {
+        println!(
+            "{:?} or {:?} is {:?}",
+            stringify!($left),
+            stringify!($right),
+            $left || $right
+        )
+    };
+}
+fn main() {
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+}
